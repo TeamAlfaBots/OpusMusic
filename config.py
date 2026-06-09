fb2b8d0ed19d1e641738df09bb4c60f629b8c6ee
TeamAlfaBots <thedeveloper413@gmail.com>
2026-06-09 09:10:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ class Config:
         ]
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
         self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/ynwsxi.png")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/m4jeoo.jpg")
+        self.START_IMG = getenv("START_VEDIO", "https://files.catbox.moe/m4jeoo.jpg")
 
     def check(self):
         missing = [
